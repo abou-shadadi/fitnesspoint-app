4af3fabdef1c6c00fc7ec84b67d3a6121709da8b
Abou-Shadadi NIYONKURU <aboushadadi@gmail.com>
2026-01-26 14:45:19 +0200

    Add files

--- config/cors.php
+++ config/cors.php
@@ -1,28 +1,39 @@
 <?php
+
 return [
 
-    'paths' => ['api/*', 'storage/*'], // Include the /storage path
+	/*
+		    |--------------------------------------------------------------------------
+		    | Cross-Origin Resource Sharing (CORS) Configuration
+		    |--------------------------------------------------------------------------
+		    |
+		    | Here you may configure your settings for cross-origin resource sharing
+		    | or "CORS". This determines what cross-origin operations may execute
+		    | in web browsers. You are free to adjust these settings as needed.
+		    |
+		    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+		    |
+	*/
 
-    'allowed_methods' => ['*'],
+	'paths' => ['api/*', 'sanctum/csrf-cookie', 'storage/*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:3002',
-        'https://api.fitnesspoint.rw'
+	'allowed_methods' => ['*'],
+
+	'allowed_origins' => ['*'],
+
+	'allowed_origins_patterns' => [],
+
+	'allowed_headers' => ['*'],
+
+    'exposed_headers' => [
+        'Content-Disposition',
+        'Content-Type',
+        'Content-Length',
     ],
 
-    'allowed_origins_patterns' => [
-        '/^https:\/\/aapi\.fitnesspoint\.rw$/',
-        '/^https:\/\/fitnesspoint\.rw$/',
-    ],
+	'supports_credentials' => false,
 
-    'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
 
-    'max_age' => 0,
-
-    'supports_credentials' => true,
 
 ];
